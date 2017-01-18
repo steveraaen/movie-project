1a2b9a745bd4e94510055f8b9e00371a74cd9085
steveraaen <sraaen@gmail.com>
2017-01-18 11:04:07 -0500

    Core logic for IMDB searches complete and described in comments

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,70 +1,53 @@
-    $('#myModal').on('load', function() {
-        // if (!data) return e.preventDefault()  stops modal from being shown
-    });
-
-    $(document).ready(function() {
+$(document).ready(function() {
+        // ===========================================================
+        // Define variables
         var searchVal;
-        var nameVal;
-        var res;
-        var des;
-        var baseAjax;
         var baseURL;
-        var nameAjax;
-        var choice;
-        var curActor;
-        var idArray = [];
+        var thisVal;
+        var actorURL;
+        var respA;
+        var resp;
+        var curChoice;
+        // ===========================================================
+        // Click button to assign input value to searchVal, construct URL and run Ajax
         $('#search').on('click', function() {
             searchVal = $('#name').val().trim();
-             baseURL = 'http://imdb.wemakesites.net/api/search?q='  + searchVal + '&api_key=cfa2962b-0e94-49f0-a266-76045337173c'
-  
-            console.log(baseURL)
+            baseURL = 'http://imdb.wemakesites.net/api/search?q=' + searchVal + '&api_key=cfa2962b-0e94-49f0-a266-76045337173c'
             baseAjax = $.ajax({
-
                 url: baseURL,
                 method: 'GET',
-                dataType: 'jsonp',
-
+                dataType: 'jsonp'
             }).done(function(responseA) {
-                res = responseA.data.results.names;
-console.log(res)
-            
-                for (let i = 0; i < res.length; i++) {                   
-                    curActor = ({
-                        title: res[i].title,
-                        id: res[i].id,
-                        thumbnail: res[i].thumbnail,
-                        nameURL : 'http://imdb.wemakesites.net/api/' + res[i].id + '?api_key=cfa2962b-0e94-49f0-a266-76045337173c'
-                    });
-                    idArray.push(curActor);
-         
-                    var strh = $('#actors').append('<tr><td class = "actor">' + res[i].title + '</td><td class = "id">' + res[i].id + '</td><td><img src ="' + res[i].thumbnail  + '" alt = "thumbnail"></td></div></tr>')
-                    /*$('.id').hide();*/
+                // ===========================================================
+                // Assign respA as shortcut to results.  Loop throgh response and display name, id & thumbnail 
+                respA = responseA.data.results.names;
+                console.log(respA)
+                for (var i = respA.length - 1; i >= 0; i--) {
+                    $('#searchContent').append(' <div class = "well row choices" id = "' + respA[i].id + '"><div class = "col-md-4">' + respA[i].title + '</div><div class = "col-md-4 actorID">' + respA[i].id + '</div><div class = "col-md-4"><img src ="' + respA[i].thumbnail + '"></div></div>')
+                    console.log(respA)
                 }
-                console.log(idArray)
-                console.log(strh)
-                });
             });
-                $('td.actor').on('click', function() {
-
-                  /*$('#films').empty()*/
-                   console.log('nameURL')
-                    nameAjax = $.ajax({
-                        url: nameURL,
-                        method: 'GET',
-                        dataType: 'jsonp'
-                    }).done(function(response) {
-                        /*console.log(response)*/
-                            /*   des = response.data.description;*/
-                            /*   console.log(response.data.description)*/
-                        films = response.data.filmography;
-                        console.log(films)
-                        $('#description').append(des)
-                        for (let i = 0; i < films.length; i++) {
-                            $('#films').append('<tr><td><a href = "' + films[i].info + '">' + films[i].title + '<td>' + films[i].year + '<td><tr>');
-
-                        }
-                    });
-                });
-            
-        
-    });
+        });
+        // ===========================================================
+        // Event handler to assign the id of person clicked to curChoice variable & construct URL for new Ajax
+        $(document).on('click', '.choices', function() {
+            curChoice = (this);
+            var thisVal = curChoice.id;
+            actorURL = 'http://imdb.wemakesites.net/api/' + thisVal + '?api_key=cfa2962b-0e94-49f0-a266-76045337173c'
+            actorAjax = $.ajax({
+                url: actorURL,
+                method: 'GET',
+                dataType: 'jsonp'
+            }).done(function(response) {
+                // ===========================================================
+                // Append description to description, loop through results & list all films            
+                $('#description').html(response.data.description);
+                for (let i = 0; i < response.data.filmography.length; i++) {
+                    $('#searchContent').append(response.data.filmography[i].title + '<br>')
+                }
+                console.log(thisVal)
+                console.log(response)
+            })
+        })
+    })
+    // 'http://imdb.wemakesites.net/api/nm0000354?api_key=cfa2962b-0e94-49f0-a266-76045337173c'
