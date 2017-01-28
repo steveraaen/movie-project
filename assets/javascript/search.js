41d2cb143926d1b74df4ffd5158085e6b905077d
Kevin Lawler <kevinrlawler@gmail.com>
2017-01-27 22:21:44 -0500

    changes from steve

--- assets/javascript/search.js
+++ assets/javascript/search.js
@@ -5,120 +5,7 @@ function show(shown, hidden) {
     return false;
 }
 $(document).ready(function() {
-<<<<<<< HEAD
 
-// ===========================================================
-// Define variables
-var searchVal;
-var baseURL;
-var actorURL;
-var respA;
-var resp;
-var curChoice;
-var thisVal;
-var currentFilm;
-var movieID
-// ===========================================================
-
-// Click button to do search on the Initial Page
-$('#submit').on('click', function() {
-InitialSearch();
-});
-//Function to make enter perform the search on the main page
-$("#actor").keypress(function (enter) {
-if (enter.keyCode === 13) {
-console.log('enter clicked');
-$('#Page1').hide();
-$('#Page2').show();
-InitialSearch();
-enter.preventDefault();
-}
-});
-//Search funtion on Page 2 to work on click
-$('#search').on('click', function() {
-$('#actor').val($('#name').val().trim());
-$('#searchContent').empty();
-InitialSearch();
-});
-//Function to make enter perform the search on the 2nd   page
-$("#name").keypress(function (enter) {
-if (enter.keyCode === 13) {
-$('#actor').val($('#name').val().trim());
-$('#searchContent').empty();
-InitialSearch();
-enter.preventDefault();
-} 
-});
-//assign input value to searchVal, construct URL and run Ajax
-function InitialSearch() {
-searchVal = $('#actor').val().trim();
-searchVal = searchVal.replace(" ", "_");
-baseURL = 'http://imdb.wemakesites.net/api/search?q=' + searchVal + '&api_key=cfa2962b-0e94-49f0-a266-76045337173c'
-baseAjax = $.ajax({
-url: baseURL,
-method: 'GET',
-dataType: 'jsonp'
-}).done(function(responseA) {
-$('#info').html("");
-// ===========================================================
-// Assign respA as shortcut to results.  Loop throgh response and display name, id & thumbnail
-respA = responseA.data.results.names;
-console.log(respA)
-for (var i = 0; i < respA.length; i++) {
-$('#searchContent').append(' <div class = "well row choices" id =" ' + respA[i].id + '" ><div class = "col-md-4">' + respA[i].title + '</div><div class = "col-md-4"><img src ="' + respA[i].thumbnail + '"><button class= "info" id = "' + respA[i].id + '" data-actorname ="' + respA[i].title + '" data-moreinfoclicked = ' + '"no"' + '"  >Click to See More/Less</button></div></div>')
-console.log(respA)
-}
-});
-}
-// ===========================================================
-// Event handler to assign the id of person clicked to curChoice variable & construct URL for new Ajax
-$(document).on('click', '.info', function() {
-if ((this).dataset.moreinfoclicked === "no") {
-$(this).attr('data-moreinfoclicked', "yes");
-curChoice = (this);
-console.log(curChoice);
-ActorName = curChoice.dataset.actorname;
-console.log(ActorName);
-//console.log(curChoice);
-console.log(curChoice.id);
-thisVal = curChoice.id;
-actorURL = 'http://imdb.wemakesites.net/api/' + thisVal + '?api_key=cfa2962b-0e94-49f0-a266-76045337173c'
-actorAjax = $.ajax({
-url: actorURL,
-method: 'GET',
-dataType: 'jsonp'
-}).done(function(response) {
-// var id = $(curChoice).attr("id");
-// ===========================================================
-// Append description to description, loop through results & list all films
-var ActorDetailsDiv = $('<div id="ActorDetailsDiv'+thisVal+'"></div>');
-var ActorDescription = $('<div class="ActorDescriptionDiv">' + "Actor Description:" + '</div>');
-ActorDescription.append('<div class="ActorDescription">' + response.data.description + '</div>');
-ActorDetailsDiv.append(ActorDescription);
-for (let i = 0; i < response.data.filmography.length; i++) {
-    currentFilm = response.data.filmography[i].title;
-    movieID = thisVal + currentFilm.replace(/\s/g, '');
-var movieListItem = $('<div class="movieListItem" id="'+movieID+'" data-moviename="' + currentFilm + '" data-actorid="'+ thisVal+'" data-actorname="' + ActorName + '"></div>');
-movieListItem.text(response.data.filmography[i].title);
-ActorDetailsDiv.append(movieListItem);
-$("#" + thisVal).parent().append(ActorDetailsDiv);
-}
-console.log(thisVal);
-console.log(response);
-console.log("about to make call");
-})
-} // end if statement for the more info call
-else {
-//toggle the amount of information shown after the initial API Call
-console.log((this).id);
-$('#ActorDetailsDiv'+ (this).id).toggle();
-}
-})
-//click function that will load the movies on the screen when a movie from the list is clicked
-$(document).on('click', '.movieListItem', function() {
-        $('.videoDiv').empty();
-
-=======
     // ===========================================================
     // Define variables
     var searchVal;
@@ -243,7 +130,6 @@ $(document).on('click', '.movieListItem', function() {
         //click function that will load the movies on the screen when a movie from the list is clicked
     $(document).on('click', '.movieListItem', function() {
         $('.videoDiv').empty();
->>>>>>> aa50ab535816d9741ddafa0299233817e049cabe
         currentFilm = (this).dataset.moviename;
         thisVal = (this).dataset.actorid;
         ActorName = (this).dataset.actorname;
@@ -266,13 +152,9 @@ $(document).on('click', '.movieListItem', function() {
                 'discoveryDocs': ['https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest'],
             }).then(function() {
                 // Executes an API request, and returns a Promise.
-<<<<<<< HEAD
 
                 // The method name `language.translations.list` comes from the API discovery.
 
-=======
-                // The method name `language.translations.list` comes from the API discovery.
->>>>>>> aa50ab535816d9741ddafa0299233817e049cabe
                 var request = gapi.client.youtube.search.list({
                     q: query,
                     part: 'snippet'
@@ -286,17 +168,12 @@ $(document).on('click', '.movieListItem', function() {
                 response.result.items.forEach(function(item) {
 
                         //add images and snippet into the videoDiv
-<<<<<<< HEAD
-                    videoDiv.append('<div id="snippet">' + item.snippet.title + '</div>');
-                    videoDiv.append('<img class="youtubeThumbnail" src="https://img.youtube.com/vi/'+item.id.videoId+'/default.jpg" data-videolink="https://www.youtube.com/embed/'+item.id.videoId+'?enablejsapi=1">');
-                   
-                })
-=======
+
                         videoDiv.append('<div id="snippet">' + item.snippet.title + '</div>');
                         videoDiv.append('<img class="youtubeThumbnail" src="https://img.youtube.com/vi/' + item.id.videoId + '/default.jpg" data-videolink="https://www.youtube.com/embed/' + item.id.videoId + '?enablejsapi=1">');
 
                     })
->>>>>>> aa50ab535816d9741ddafa0299233817e049cabe
+
                     //append the videoDiv to the DOM
                 $('#' + movieID).append(videoDiv);
                 //$('#' + thisVal).parent().append(videoDiv);
@@ -310,18 +187,6 @@ $(document).on('click', '.movieListItem', function() {
     }
 
 
-<<<<<<< HEAD
-        //Clears out the modal link when you click away from the player and stops background audio
-$('#modal-video').on('hidden.bs.modal', function () {
-  $('#modalplayer').attr("src","");
-})
-
-    $(document).on('click', '.youtubeThumbnail', function(e){
-
-        e.preventDefault();
-        console.log((this).dataset.videolink);
-        $('#modalplayer').attr("src",(this).dataset.videolink);
-=======
     //Clears out the modal link when you click away from the player and stops background
 
     $('#modal-video').on('hidden.bs.modal', function() {
@@ -332,13 +197,10 @@ $('#modal-video').on('hidden.bs.modal', function () {
         e.preventDefault();
         console.log((this).dataset.videolink);
         $('#modalplayer').attr("src", (this).dataset.videolink);
->>>>>>> aa50ab535816d9741ddafa0299233817e049cabe
+
         console.log((this).dataset.videolink);
         $('#modal-video').modal();
     });
 
-<<<<<<< HEAD
 });
-=======
-});
->>>>>>> aa50ab535816d9741ddafa0299233817e049cabe
+
