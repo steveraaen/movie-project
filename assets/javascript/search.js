aa50ab535816d9741ddafa0299233817e049cabe
klawgolfer7 <kevinrlawler@gmail.com>
2017-01-27 20:43:42 -0500

    front end changes to make it look more presentable. Backend is functional.

--- assets/javascript/search.js
+++ assets/javascript/search.js
@@ -15,16 +15,27 @@ $(document).ready(function() {
     var curChoice;
     var thisVal;
     var currentFilm;
-    // ===========================================================
-
+    var movieID;
+    var goodPic;
+        // ===========================================================
+  
+      $(".element").typed({
+        showCursor: false,
+        strings: ["Like movies?  We do!  IMDB is the holy grail of movie info.", "Too bad IMDB doesn't let you watch a movie's trailer.", "Fret no more, movie lover.  Limelight is here!"],
+        typeSpeed: 0
+      });
+ 
     // Click button to do search on the Initial Page
     $('#submit').on('click', function() {
         InitialSearch();
+        $('#top').hide();
     });
     //Function to make enter perform the search on the main page
     $("#actor").keypress(function(enter) {
+
         if (enter.keyCode === 13) {
             console.log('enter clicked');
+
             $('#Page1').hide();
             $('#Page2').show();
             InitialSearch();
@@ -33,19 +44,20 @@ $(document).ready(function() {
     });
     //Search funtion on Page 2 to work on click
     $('#search').on('click', function() {
+
         $('#actor').val($('#name').val().trim());
         $('#searchContent').empty();
         InitialSearch();
     });
     //Function to make enter perform the search on the 2nd   page
-    $("#name").keypress(function(enter) {
+/*    $("#name").keypress(function(enter) {
         if (enter.keyCode === 13) {
             $('#actor').val($('#name').val().trim());
             $('#searchContent').empty();
             InitialSearch();
             enter.preventDefault();
         }
-    });
+    });*/
     //assign input value to searchVal, construct URL and run Ajax
     function InitialSearch() {
         searchVal = $('#actor').val().trim();
@@ -62,7 +74,7 @@ $(document).ready(function() {
             respA = responseA.data.results.names;
             console.log(respA)
             for (var i = 0; i < respA.length; i++) {
-                $('#searchContent').append(' <div class = "well row choices" id =" ' + respA[i].id + '" ><div class = "col-md-4">' + respA[i].title + '</div><div class = "col-md-4"><img src ="' + respA[i].thumbnail + '"><button class= "info" id = "' + respA[i].id + '" data-actorname ="' + respA[i].title + '" data-moreinfoclicked = ' + '"no"' + '"  >Click to See More/Less</button></div></div>')
+                $('#searchContent').append(' <div class = "well row choices" id =" ' + respA[i].id + '" ><div>' + respA[i].title + '</div><div class = "col-md-12"><img src ="' + respA[i].thumbnail + '"><button class= "info" id = "' + respA[i].id + '" data-actorname ="' + respA[i].title + '" data-moreinfoclicked = ' + '"no"' + '"  >Click to See More/Less</button></div></div>')
                 console.log(respA)
             }
         });
@@ -89,17 +101,22 @@ $(document).ready(function() {
                     // ===========================================================
                     // Append description to description, loop through results & list all films
                     var ActorDetailsDiv = $('<div id="ActorDetailsDiv' + thisVal + '"></div>');
-                    var ActorDescription = $('<div class="ActorDescriptionDiv">' + "Actor Description:" + '</div>');
+                    var ActorDescription = $('<div class="ActorDescriptionDiv">' + "Actor Description:<br><br>" + '</div>');
                     ActorDescription.append('<div class="ActorDescription">' + response.data.description + '</div>');
                     ActorDetailsDiv.append(ActorDescription);
                     for (let i = 0; i < response.data.filmography.length; i++) {
-                        var movieListItem = $('<div class="movieListItem" data-moviename="' + response.data.filmography[i].title + '" data-actorid="' + thisVal + '"></div>');
+                        goodPic = response.data.image;
+                        currentFilm = response.data.filmography[i].title;
+                        movieID = thisVal + currentFilm.replace(/\s/g, '');
+                        var movieListItem = $('<div class="row movieListItem well"></div>');
                         movieListItem.text(response.data.filmography[i].title);
+                        movieListItem.attr('src', response.data.image);
                         ActorDetailsDiv.append(movieListItem);
                         $("#" + thisVal).parent().append(ActorDetailsDiv);
+                        console.log(response);
                     }
                     console.log(thisVal);
-                    console.log(response);
+                    
                     console.log("about to make call");
                 })
             } // end if statement for the more info call
@@ -111,12 +128,15 @@ $(document).ready(function() {
         })
         //click function that will load the movies on the screen when a movie from the list is clicked
     $(document).on('click', '.movieListItem', function() {
-        $('.video').empty();
+        $('.videoDiv').empty();
         currentFilm = (this).dataset.moviename;
         thisVal = (this).dataset.actorid;
+        ActorName = (this).dataset.actorname;
+        movieID = (this).id;
         console.log(currentFilm);
         //$(this).siblings().dataset.moviename.toggle();
         youtubeCall();
+
     });
 
     function youtubeCall() {
@@ -131,7 +151,7 @@ $(document).ready(function() {
                 'discoveryDocs': ['https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest'],
             }).then(function() {
                 // Executes an API request, and returns a Promise.
-                // the methon name "language-tranlation.list" comes from the API discovery
+                // The method name `language.translations.list` comes from the API discovery.
                 var request = gapi.client.youtube.search.list({
                     q: query,
                     part: 'snippet'
@@ -141,24 +161,37 @@ $(document).ready(function() {
                 console.log(response.result.items);
                 var videoDiv = $('<div class="videoDiv">');
                 response.result.items.forEach(function(item) {
-                    var video = $('<span class="video">');
-                    video.append('<iframe class="youtubePlayer" width="130" height="auto" src="https://www.youtube.com/embed/' + item.id.videoId + '" frameborder="0" allowfullscreen></iframe>');
-                    videoDiv.append(video);
-                })
-                $('#' + thisVal).parent().append(videoDiv);
+
+                        //add images and snippet into the videoDiv
+                        videoDiv.append('<div id="snippet">' + item.snippet.title + '</div>');
+                        videoDiv.append('<img class="youtubeThumbnail" src="https://img.youtube.com/vi/' + item.id.videoId + '/default.jpg" data-videolink="https://www.youtube.com/embed/' + item.id.videoId + '?enablejsapi=1">');
+
+                    })
+                    //append the videoDiv to the DOM
+                $('#' + movieID).append(videoDiv);
+                //$('#' + thisVal).parent().append(videoDiv);
+
             }, function(reason) {
                 console.log('Error: ' + reason.result.error.message);
             });
         };
-        // Loads the JavaScript client library and invokes start afterwards.
+        // Loads the JavaScript client library and invokes `start` afterwards.
         gapi.load('client', start);
     }
-    $('.youtubePlayer').on('onStateChange', function() {
 
-        console.log("hello");
-    });
-    $(document).on('click', '.videoDiv', function(e) {
+
+    //Clears out the modal link when you click away from the player and stops background
+
+    $('#modal-video').on('hidden.bs.modal', function() {
+        $('#modalplayer').attr("src", "");
+    })
+
+    $(document).on('click', '.youtubeThumbnail', function(e) {
         e.preventDefault();
+        console.log((this).dataset.videolink);
+        $('#modalplayer').attr("src", (this).dataset.videolink);
+        console.log((this).dataset.videolink);
         $('#modal-video').modal();
     });
+
 });
